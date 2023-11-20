76305f9a0beb4b90086462f9aa21ad6434f7893f
Vinicius Martinez <vn.martinez@hotmail.com>
2023-11-20 20:02:17 -0300

    teste

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from fastapi import FastAPI
 from fastapi import APIRouter
-from src.endpoints.pessoa import router as pessoa_router
-from src.endpoints.publicacao import router as publicacao_router
-from src.endpoints.auth import router as auth_router
-from src.database import  Base, engine
-from src.models.user import User
+#from src.endpoints.pessoa import router as pessoa_router
+#from src.endpoints.publicacao import router as publicacao_router
+#from src.endpoints.auth import router as auth_router
+#from src.database import  Base, engine
+#from src.models.user import User
 
-Base.metadata.create_all(bind=engine)
+#Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
-app.include_router(auth_router, prefix="/auth", tags=["login"])
+@app.get("/")
+async def root():
+    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
 
-app.include_router(pessoa_router, prefix="/pessoa", tags=["pessoa"])
+#app.include_router(auth_router, prefix="/auth", tags=["login"])
 
-app.include_router(publicacao_router, prefix="/publicacao", tags=["publicacao"])
+#app.include_router(pessoa_router, prefix="/pessoa", tags=["pessoa"])
+
+#app.include_router(publicacao_router, prefix="/publicacao", tags=["publicacao"])
