9b3259b65585cd17b3abc205056c8cfe9ddad28d
Vinicius Martinez <vn.martinez@hotmail.com>
2023-12-05 21:11:05 -0300

    get nome da pessoa no comentario

--- src/endpoints/comentarios.py
+++ src/endpoints/comentarios.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session, joinedload
 from src.models.publicacao import Publicacao
 from src.dependencies import get_db
 from src.helpers.auth_valid import oauth2_scheme, get_current_user
-from src.schemas.comentario import comentarioCreate
+from src.schemas.comentario import comentarioCreate, getComentarioLista, getComentario
 from src.models.comentario import comentarios
 from src.models.pessoa import Pessoa
 from sqlalchemy import func
@@ -21,11 +21,17 @@ async def Comentar_Publicacao(publicacao_id: int, comentario: comentarioCreate, 
      db.commit()
      return {"mensagem": "Comentário adicionado com sucesso!"}
 
-@router.get("/{publicacao_id}")
+@router.get("/{publicacao_id}", response_model = getComentarioLista)
 async def Listar_Comentarios(publicacao_id: int, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
      publicacao = db.query(Publicacao).filter(Publicacao.id == publicacao_id).first()
-     pessoa_nome = db.query(func.concat(Pessoa.nome, ' ', Pessoa.sobrenome).label('nome_completo')).filter(Pessoa.id == comentarios.id_pessoa).first()
      if not publicacao:
-          raise HTTPException(status_code=404, detail="Publicação não encontrada")
-     lista_comentarios = db.query(comentarios).filter(comentarios.id_publicacao == publicacao_id).all()
-     return {"comentarios": lista_comentarios, "pessoa" : pessoa_nome}
+        raise HTTPException(status_code=404, detail="Publicação não encontrada")
+
+     lista_comentarios = db.query(comentarios.id, comentarios.texto_comentario, comentarios.id_pessoa, func.concat(Pessoa.nome,' ', Pessoa.sobrenome).label("nome_pessoa"), comentarios.id_publicacao
+                                  ).join(Pessoa, comentarios.id_pessoa == Pessoa.id
+                                  ).filter(comentarios.id_publicacao == publicacao_id).all()
+
+     if not lista_comentarios:
+        raise HTTPException(status_code=404, detail="Não há comentários para esta publicação")
+
+     return {"comentarios": lista_comentarios}
--- src/schemas/comentario.py
+++ src/schemas/comentario.py
@@ -1,5 +1,16 @@
 from pydantic import BaseModel
 from datetime import date
+from typing import List
 
 class comentarioCreate(BaseModel):
     texto_comentario : str
+    
+class getComentario(BaseModel):
+    id : int
+    texto_comentario :str
+    id_pessoa : int
+    nome_pessoa : str
+    id_publicacao  : int
+    
+class getComentarioLista(BaseModel):
+    comentarios : List[getComentario]
